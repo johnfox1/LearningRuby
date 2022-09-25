7d836554d6853cd6b1e9aa5337d4fdd2caf920e2
John Fox <johnfox1@gmail.com>
2022-09-25 19:45:23 +0000

    Ch7 Hashes - Exercise on page 229

---
+++ Ch7 - Hashes/Ch7_exercise_p229_v2.rb
@@ -0,0 +1,10 @@
+# Ch7 Hashes - Exercise on p229
+# 26th Sept, 2022
+
+my_hash = {"one" => "two", :three => "four", 5 => "six"}
+puts my_hash[5]
+puts my_hash["one"]
+puts my_hash[:three]
+my_hash["seven"] = 8
+puts my_hash["seven"]
+
