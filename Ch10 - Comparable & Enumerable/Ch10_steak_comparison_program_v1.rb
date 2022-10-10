8db00cf99a5e2ec8360760de96575e552eba5896
John Fox <johnfox1@gmail.com>
2022-10-10 01:35:21 +0000

    Ch10 - Steak comparison app v1

---
+++ Ch10 - Comparable & Enumerable/Ch10_steak_comparison_program_v1.rb
@@ -0,0 +1,27 @@
+# Ch10 Comparable & Enumerable - Steak comparison progragm v1
+# 10th Oct, 2022
+
+# Setting up a class for Steak
+class Steak
+  attr_accessor :grade
+
+  def > (other)
+    grade_scores = {
+      "Prime" => 3,
+      "Choice" => 2,
+      "Select" => 1
+    }
+    grade_scores[grade] > grade_scores[other.grade]
+  end
+
+end
+
+first_steak = Steak.new
+first_steak.grade = "Prime"
+second_steak = Steak.new
+second_steak.grade = "Choice"
+
+if first_steak > second_steak
+  puts "I'll take #{first_steak.inspect}"
+end  
+  
