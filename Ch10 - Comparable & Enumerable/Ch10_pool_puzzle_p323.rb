d10adf0ea67ec5fe716171a97764d5d625eac0cc
John Fox <johnfox1@gmail.com>
2022-10-11 20:08:59 +0000

    Pool puzzle on page 323

---
+++ Ch10 - Comparable & Enumerable/Ch10_pool_puzzle_p323.rb
@@ -0,0 +1,25 @@
+# Ch10 - Pool puzzle on page 323
+# 12th Oct, 2022
+
+class Apple
+  include Comparable
+  attr_accessor :weight
+
+  def initialize(weight)
+    self.weight = weight
+  end
+
+  def <=> (other)
+    self.weight <=> other.weight
+  end
+
+end
+
+small_apple = Apple.new(0.17)
+medium_apple = Apple.new(0.22)
+big_apple = Apple.new(0.25)
+
+puts "small_apple > medium_apple: "
+puts small_apple > medium_apple
+puts "medium_apple < big_apple: "
+puts medium_apple < big_apple
